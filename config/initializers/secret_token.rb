ab34e9809800a721e74dfe06ffcb5f0293e31c0b
Ismar Dantas <ismardantasbs@hotmail.com>
2012-11-20 20:27:03 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bradesco::Application.config.secret_token = '089a4ccb690100528e9c810496ee935c34a0dcb9a93cad90f38bcd0055c358980bb5fe76a6acd93619254e77462bfbcdafad59b078f3c59ef81cf33bc6773914'
